aecde0760b0492c89511a35df29493b21430d17c
Vlad <147541054+kvs23031999@users.noreply.github.com>
2023-11-08 12:29:49 +0200

    second commit

--- src/HWProgram20.java
+++ src/HWProgram20.java
@@ -3,13 +3,18 @@ import java.util.Date;
 
 public class HWProgram20 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        User user1 = new User("Sergei",1972,6,18,"kvs18061972@gmail.com","0954138982","Kulishov",100,120,1000,50);
-        User user2 = new User("Natalia",1973,9,10,"kne10091973@gmail.com","0505771526","Kulishova",60,120,1500,50);
-        User user3 = new User("Vladyslav",1999,3,23,"kvs230398@gmail.com","0685503985","Kulishov",95,130,2000,24);
-        User user4 = new User("Artem",2012,12,25,"kac25122012@gmail.com","0950298373","Kulishov",40,110,500,10);
-        User user5 = new User("Anastasia",2001,7,12,"aburlachka7@gmail.com","0669531216","Burlachka",60,120,2500,22);
+        User user1 = new User("Sergei", 1972, 6, 18, "kvs18061972@gmail.com", "0954138982",
+                "Kulishov", 100, 120, 1000);
+        User user2 = new User("Natalia", 1973, 9, 10, "kne10091973@gmail.com", "0505771526",
+                "Kulishova", 60, 120, 1500);
+        User user3 = new User("Vladyslav", 1999, 3, 23, "kvs230398@gmail.com", "0685503985",
+                "Kulishov", 95, 130, 2000);
+        User user4 = new User("Artem", 2012, 12, 25, "kac25122012@gmail.com", "0950298373",
+                "Kulishov", 40, 110, 500);
+        User user5 = new User("Anastasia", 2001, 7, 12, "aburlachka7@gmail.com", "0669531216",
+                "Burlachka", 60, 120, 2500);
         user1.printAccountInfo();
         user2.printAccountInfo();
         user3.printAccountInfo();
@@ -17,15 +22,15 @@ public class HWProgram20 {
         user5.printAccountInfo();
 
         user1.setSurname("Kulishov");
-        user1.setWeight(100);
-        user1.setPressure(120);
-        user1.setSteps(1000);
+        user1.setWeight(90);
+        user1.setPressure(110);
+        user1.setSteps(1500);
 
         user2.setSurname("Kulishova");
-        user2.setWeight(60);
+        user2.setWeight(65);
         user2.setPressure(134);
-        user2.setSteps(1500);
-
+        user2.setSteps(1800);
+        System.out.println("Змінені дані");
         user1.printAccountInfo();
         user2.printAccountInfo();
     }
--- src/User.java
+++ src/User.java
@@ -12,7 +12,8 @@ public class User {
     private int steps;
     private int age;
 
-    public User(String name, int years, int month, int day, String email, String phone, String surname, int weight, int pressure, int steps, int age) {
+    public User(String name, int years, int month, int day, String email, String phone, String surname, int weight,
+                int pressure, int steps) {
         this.name = name;
         this.years = years;
         this.month = month;
@@ -23,71 +24,88 @@ public class User {
         this.weight = weight;
         this.pressure = pressure;
         this.steps = steps;
-        this.age = age;
+        this.age = 2023 - years;
     }
 
     public String getName() {
+
         return name;
     }
 
     public int getYears() {
+
         return years;
     }
 
     public int getMonth() {
+
         return month;
     }
 
     public int getDay() {
+
         return day;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public String getPhone() {
+
         return phone;
     }
 
     public String getSurname() {
+
         return surname;
     }
 
     public int getWeight() {
+
         return weight;
     }
 
     public int getPressure() {
+
         return pressure;
     }
 
     public int getSteps() {
+
         return steps;
     }
 
     public int getAge() {
+
         return age;
     }
 
     public void setSurname(String surname) {
+
         this.surname = surname;
     }
 
     public void setWeight(int weight) {
+
         this.weight = weight;
     }
 
     public void setPressure(int pressure) {
+
         this.pressure = pressure;
     }
 
     public void setSteps(int steps) {
+
         this.steps = steps;
     }
 
     public void printAccountInfo() {
-        System.out.println(" Name " + name + " years " + years + " Month " + month + " day " + day + " email " + email + " phone " + phone + " surname " + surname + " weight " + weight + " pressure " + pressure + " steps " + steps + " age " + age);
+        System.out.println(" Name " + name + " years " + years + " Month " + month + " day " + day + " email " + email +
+                " phone " + phone + " surname " + surname + " weight " + weight + " pressure " + pressure + " steps " +
+                steps + " age " + age);
 
     }
 }
